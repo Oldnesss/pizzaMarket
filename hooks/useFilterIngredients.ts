f977b0dc51c2336f9f040636ff4d6ab401372508
Oldnesss <viv251985@gmail.com>
2024-09-26 07:05:52 +0300

    add checkbox to filters

--- hooks/useFilterIngredients.ts
+++ hooks/useFilterIngredients.ts
@@ -1,24 +1,35 @@
 import { Api } from "@/services/api-client";
 import { Ingredient } from "@prisma/client";
 import React from "react";
+import { useSet } from "react-use";
 
 // type IngredientItem = Pick<Ingredient, "id" | "name">;
 interface ReturnProps {
   ingredients: Ingredient[];
+  loading: boolean;
+  selected: Set<string>;
+  onAddId: (id: string) => void;
 }
 
 export const useFilterIngredients = (): ReturnProps => {
   const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
+  const [loading, setLoading] = React.useState(true);
+
+  const [selected, { toggle }] = useSet(new Set<string>([]));
+
   React.useEffect(() => {
     async function fetchIngredients() {
       try {
+        setLoading(true);
         const ingredients = await Api.ingredients.getAll();
         setIngredients(ingredients);
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false);
       }
     }
     fetchIngredients();
   }, []);
-  return { ingredients };
+  return { ingredients, loading, onAddId: toggle, selected };
 };
