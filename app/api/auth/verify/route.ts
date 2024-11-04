58eb97ee0e1d98485438bdc07c37214b1edab921
Oldnesss <viv251985@gmail.com>
2024-11-04 12:22:42 +0300

    fix verify

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,19 +1,18 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"; // Указание на динамическую обработку маршрута
+
 export async function GET(req: NextRequest) {
   try {
-    const code = req.nextUrl.searchParams.get('code');
-    // const code = "";
+    const code = req.nextUrl.searchParams.get("code");
 
     if (!code) {
       return NextResponse.json({ error: "Неверный код" }, { status: 400 });
     }
 
     const verificationCode = await prisma.verificationCode.findFirst({
-      where: {
-        code,
-      },
+      where: { code },
     });
 
     if (!verificationCode) {
@@ -21,23 +20,25 @@ export async function GET(req: NextRequest) {
     }
 
     await prisma.user.update({
-      where: {
-        id: verificationCode.userId,
-      },
-      data: {
-        verified: new Date(),
-      },
+      where: { id: verificationCode.userId },
+      data: { verified: new Date() },
     });
 
     await prisma.verificationCode.delete({
-      where: {
-        id: verificationCode.id,
-      },
+      where: { id: verificationCode.id },
     });
 
-    return NextResponse.redirect(new URL("/?verified", req.url));
+    try {
+      return NextResponse.redirect(new URL("/?verified", req.url));
+    } catch (redirectError) {
+      console.error("Ошибка редиректа:", redirectError);
+      return NextResponse.json({ error: "Ошибка редиректа" }, { status: 500 });
+    }
   } catch (error) {
-    console.error(error);
-    console.log("[VERIFY_GET] Server error", error);
+    console.error("[VERIFY_GET] Server error", error);
+    return NextResponse.json(
+      { error: "Произошла ошибка на сервере" },
+      { status: 500 }
+    );
   }
 }
